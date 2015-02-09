1692fe97c66e27ad886bb5b43672dec80c0aa3dd
David Shah <david.a.shah@gmail.com>
2015-02-08 20:28:05 -0500

    Becuase, always use fragments

--- android/TVBGone/app/src/main/java/com/burningriverbitworks/tvbgone/TVBGoneActivity.java
+++ android/TVBGone/app/src/main/java/com/burningriverbitworks/tvbgone/TVBGoneActivity.java
@@ -1,7 +1,7 @@
 package com.burningriverbitworks.tvbgone;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,28 +12,24 @@ public class TVBGoneActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tvbgone);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_placeholder, TVBGoneFragment.newInstance())
+                .commitAllowingStateLoss();
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_tvbgone, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
